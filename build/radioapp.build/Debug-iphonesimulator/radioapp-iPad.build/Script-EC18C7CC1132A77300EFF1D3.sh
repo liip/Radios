aa70c594349a224115d9d1a255b92ef167212af0
Christian Stocker <me@chregu.tv>
2010-02-22 13:30:56 +0100

    remove build directory....

--- build/radioapp.build/Debug-iphonesimulator/radioapp-iPad.build/Script-EC18C7CC1132A77300EFF1D3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-make -C "${PHONEGAPLIB}"
-echo cp ${PHONEGAPLIB}/javascripts/phonegap.js ${PROJECT_DIR}/www/phonegap.js
-cp "${PHONEGAPLIB}/javascripts/phonegap.js" "${PROJECT_DIR}/www/phonegap.js"
